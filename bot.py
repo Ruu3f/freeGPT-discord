af8ecfeff4aecfd2c52920486bdd09cd1bafb5fe
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-07-06 21:01:11 +0530

    Documented, optimized and fixed

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
-import discord, asyncio, aiosqlite, freeGPT
+import asyncio
+import discord
+import freeGPT
+import aiosqlite
 from discord.ext import commands
 from discord import app_commands
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -8,23 +12,23 @@ intents.message_content = True
 bot = commands.Bot(
     command_prefix="i don't want to set one...", intents=intents, help_command=None
 )
+
 models = ["gpt3", "gpt4", "alpaca_7b"]
+db = None
 
 
 @bot.event
 async def on_ready():
+    """
+    Performs necessary actions when the bot is ready.
+    """
     db = await aiosqlite.connect("database.db")
-    await db.execute(
-        """
-        CREATE TABLE IF NOT EXISTS datastorage (
-            guilds INTEGER,
-            channels INTEGER,
-            model TEXT
+    async with db.cursor() as cursor:
+        await cursor.execute(
+            "CREATE TABLE IF NOT EXISTS datastorage(guilds INTEGER, channels INTEGER, model TEXT)"
         )
-        """
-    )
     await db.commit()
-    print(f"{bot.user.name} has connected to Discord.")
+    print(f"{bot.user.name} connected to Discord.")
     sync_commands = await bot.tree.sync()
     print(f"Synced {len(sync_commands)} command(s).")
     while True:
@@ -40,6 +44,9 @@ async def on_ready():
 
 @bot.tree.command(name="help", description="Get help.")
 async def help(interaction):
+    """
+    Sends an embed with bot information.
+    """
     embed = discord.Embed(
         title="Help Menu",
         description=f"Available models: `{', '.join(models)}`",
@@ -47,9 +54,9 @@ async def help(interaction):
     )
     embed.add_field(
         name="setup",
-        value="Usage:\n- `/setup {model}`",
+        value="Usage: `/setup {model}`",
     )
-    embed.add_field(name="reset", value="Usage:\n- `/reset`")
+    embed.add_field(name="reset", value="Usage: `/reset`")
     view = discord.ui.View()
     view.add_item(
         discord.ui.Button(
@@ -77,12 +84,15 @@ async def help(interaction):
 @app_commands.checks.bot_has_permissions(manage_channels=True)
 @app_commands.describe(model=f"Model to use. Choose between {', '.join(models)}")
 async def setup(interaction, model: str):
+    """
+    Set up the chatbot.
+    """
     if model.lower() not in models:
         await interaction.response.send_message(
-            f"**Error:** Model not found! Please choose a model between `{', '.join(models)}`."
+            f"**Error:** Model not found! Choose a model between `{', '.join(models)}`."
         )
         return
-    db = await aiosqlite.connect("database.db")
+
     cursor = await db.execute(
         "SELECT channels, model FROM datastorage WHERE guilds = ?",
         (interaction.guild.id,),
@@ -90,14 +100,15 @@ async def setup(interaction, model: str):
     data = await cursor.fetchone()
     if data:
         await interaction.response.send_message(
-            "**Error:** The chatbot is already set up. Try using the `/reset` command to fix this error."
+            "**Error:** The chatbot is already set up. Use the `/reset` command to fix this error."
         )
         return
 
     if model.lower() in models:
         channel = await interaction.guild.create_text_channel(
-            f"{model}-chatbot", slowmode_delay=10
+            f"{model}-chat", slowmode_delay=10
         )
+
         await db.execute(
             "INSERT OR REPLACE INTO datastorage (guilds, channels, model) VALUES (?, ?, ?)",
             (
@@ -112,14 +123,16 @@ async def setup(interaction, model: str):
         )
     else:
         await interaction.response.send_message(
-            f"**Error:** Model not found! Please choose a model between `{', '.join(models)}`."
+            f"**Error:** Model not found! Choose a model between `{', '.join(models)}`."
         )
 
 
 @bot.tree.command(name="reset", description="Reset the chatbot.")
 @app_commands.checks.has_permissions(manage_channels=True)
 async def reset(interaction):
-    db = await aiosqlite.connect("database.db")
+    """
+    Resets the chatbot.
+    """
     cursor = await db.execute(
         "SELECT channels, model FROM datastorage WHERE guilds = ?",
         (interaction.guild.id,),
@@ -127,7 +140,7 @@ async def reset(interaction):
     data = await cursor.fetchone()
     if not data:
         await interaction.response.send_message(
-            "**Error:** The chatbot is not set up. Try using the `/setup` command to fix this error."
+            "**Error:** The chatbot is not set up. Use the `/setup` command to fix this error."
         )
         return
 
@@ -140,9 +153,12 @@ async def reset(interaction):
 
 @bot.event
 async def on_message(message):
+    """
+    Reply to the message if the bot is set up.
+    """
     if message.author == bot.user:
         return
-    db = await aiosqlite.connect("database.db")
+
     cursor = await db.execute(
         "SELECT channels, model FROM datastorage WHERE guilds = ?", (message.guild.id,)
     )
@@ -157,9 +173,9 @@ async def on_message(message):
                         prompt=message.content
                     )
                     await message.reply(resp)
-                except Exception as e:
-                    await message.reply(e)
+                except Exception:
+                    await message.reply(Exception)
 
 
 TOKEN = ""
-bot.run(TOKEN)
+asyncio.run(bot.run(TOKEN))
